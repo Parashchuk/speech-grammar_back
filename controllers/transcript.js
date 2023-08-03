58064ffba993cfee815730ae8cf4ea017ff6dacb
Parashchuk <Andry.parashchuk@gmail.com>
2023-08-03 13:54:50 +0200

    implement load voice feature

---
+++ controllers/transcript.js
@@ -0,0 +1,20 @@
+import ffmpeg from 'ffmpeg';
+
+const transcript = (req, res) => {
+  try {
+    const pathToFile = 'temp/' + req.file.filename;
+    let process = new ffmpeg(pathToFile);
+
+    process.then((audio) => {
+      audio.fnExtractSoundToMP3(pathToFile.replace('webm', 'mp3'), (error) => {
+        if (error) console.log(error);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  return res.status(200).json({ message: 'success' });
+};
+
+export default transcript;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import multer from 'multer';
 import cors from 'cors';
 import 'dotenv/config';
 
 import root from './controllers/root.js';
 import correctMessage from './controllers/correctMessage.js';
+import transcript from './controllers/transcript.js';
 
 //Create express app
 const PORT = process.env.PORT || 4000;
@@ -11,12 +13,25 @@ const app = express();
 
 //Configure express app
 app.use(express.json());
+app.use(express.static('temp'));
 app.use(cors({ origin: ['http://localhost:3000', 'https://voice-grammar-parashchuk.vercel.app'] }));
+
+//Configure multer
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'temp/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '.webm');
+  },
+});
+
+let upload = multer({ storage });
 
 //App routes
 app.get('/', root);
-app.post('/transcriptAudio', () => {});
 app.post('/correctMessage', correctMessage);
+app.post('/transcript', upload.single('audio'), transcript);
 
 //Start server
 app.listen(PORT, () => {
