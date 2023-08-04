34827f2c4776e948cf8ccc90ecda86fd30e68e09
Parashchuk <Andry.parashchuk@gmail.com>
2023-08-04 11:23:15 +0200

    update cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://voice-grammar.vercel.app'],
+    origin: ['http://localhost:3000', 'https://speech-grammar.vercel.app'],
   })
 );
 
